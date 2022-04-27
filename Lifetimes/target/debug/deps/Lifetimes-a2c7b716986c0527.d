016512c7dbe6a460795581dfbb9cc5e6a916978b
Krishnan Nath <krishnan2784@gmail.com>
2022-04-27 11:17:18 +0100

    Copy and return files

---
+++ Lifetimes/target/debug/deps/Lifetimes-a2c7b716986c0527.d
@@ -0,0 +1,6 @@
+/Users/surendranath/Work/practice/Rust-Learning/Lifetimes/target/debug/deps/Lifetimes-a2c7b716986c0527: src/main.rs src/copy_and_return.rs
+
+/Users/surendranath/Work/practice/Rust-Learning/Lifetimes/target/debug/deps/Lifetimes-a2c7b716986c0527.d: src/main.rs src/copy_and_return.rs
+
+src/main.rs:
+src/copy_and_return.rs:
